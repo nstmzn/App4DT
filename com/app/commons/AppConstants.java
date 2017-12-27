e9ee8ca98f061614bfd5c9f58487fcf1e8532831
nstmzn <nstmzn@gmail.com>
2017-12-27 20:14:44 +0530

    Create AppConstants.java

---
+++ com/app/commons/AppConstants.java
@@ -0,0 +1,41 @@
+package com.app.commons;
+
+public class AppConstants {
+	
+	public static final String IF_SUCCESS = "IF_SUCCESS";
+	public static final String IF_FAILURE = "IF_FAILURE";
+	public static final String ALWAYS = "ALWAYS";
+	
+	public static final String TASK_STATUS_ACTIVE = "ACTIVE";
+	public static final String TASK_STATUS_INACTIVE = "INACTIVE";
+	
+	public static final String aa = "aa";
+	public static final String bb = "bb";
+	public static final String cc = "cc";
+	public static final String dd = "dd";
+	
+	public static final String ACTION_DELETE = "ACTION_DELETE";
+	public static final String ACTION_RENAME = "ACTION_RENAME";
+	public static final String ACTION_MOVE = "ACTION_MOVE";
+	
+	public static final String ACTION_SUFFIX_DATE = "ACTION_SUFFIX_DATE";
+
+	
+	public static String aa_URL = "jdbc:oracle:thin:@";
+	public static String aa_USER = "";
+	public static String aa_PWD = "";
+	
+	public static String bb_URL = "jdbc:oracle:thin:@";
+	public static String bb_USER = "";
+	public static String bb_PWD = "";
+	
+	public static final String cc_CONN_URL = "jdbc:oracle:thin:@";
+	public static final String cc_USER = "";
+	public static final String cc_PWD = "";
+		
+	public static final String dd_CONN_URL = "jdbc:oracle:thin:@";
+	public static final String dd_USER = "";
+	public static final String ddK_PWD = "";
+
+	
+}
